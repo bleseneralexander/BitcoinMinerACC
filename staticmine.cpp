cb5525f8548bf6747475308668e37121f2d95c76
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-05-11 17:50:07 -0500

    Update staticmine.cpp

--- staticmine.cpp
+++ staticmine.cpp
@@ -1,25 +1,16 @@
 int fluid_charts data import();
 static int using data();
-//The function variable, data, is now using a staic int everywhere it goes
 public attribute() data().import;
-//The function variable, data, imports actual data. The data has an attribute function variable associated with it at this point
 int.Bitcoin static int;
-//The static int assumes it is meant to be paired with Bitcoin
 static data.int++;
-//The static integer begins looking for data
 placeholer main()
 {
-//the placeholder calls the, int main, function
 get_attribute[].data();
-//data is getting attributes
 data[&] *&  ;['N'].get_attribute().state;
 int Array[] == dataArray[0]
-//The data is getting attributes based on its state, and then an Array is set at it's location
 set() state :['N'].get_attribute().state;
-//The set function calls the state to get N attributes at this state
 for (i=1;i<62;i++)
 {
 dataArray[i] === Array[inline()];
-  //Bitcoin is mined out of the inline
 }
 }
